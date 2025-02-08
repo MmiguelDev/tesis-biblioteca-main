b816445b0545502ba91531c4daf21deaf116664a
MmiguelDev <22308051220620@cbtis122.edu.mx>
2025-02-08 15:06:22 -0600

    Socket.io desinstalado

--- servidor/src/index.js
+++ servidor/src/index.js
@@ -2,8 +2,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { createServer } from "http";
-import { Server } from "socket.io";
 
 /* Rutas */
 import addRouter from "./routes/add.routes.js";
@@ -18,31 +16,10 @@ dotenv.config();
 
 const app = express(); // Inicializamos Express
 const port = 5000; // Puerto
-const server = createServer(app); // Creamos un servidor HTTP
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173", // Permitir el acceso del frontend
-    credentials: true,
-  },
-});
 
 /* Hacemos que sea JSON la respuesta y que pueda acceder el frontend al backend */
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-
-/* Configurar WebSockets */
-io.on("connection", (socket) => {
-  console.log("Nuevo cliente conectado:", socket.id);
-
-  socket.on("mensaje", (data) => {
-    console.log("Mensaje recibido:", data);
-    io.emit("mensaje", data); // Reenvía el mensaje a todos los clientes
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Cliente desconectado:", socket.id);
-  });
-});
 
 /* Rutas */
 app.use("/api/add", addRouter);
@@ -59,6 +36,6 @@ app.use("*", (req, res) => {
 });
 
 /* Escuchamos el servidor con Socket.io */
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Server is running on: " + port);
 });
